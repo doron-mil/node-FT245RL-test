6188d1637f2517636ad7035a54f4ca5f0513719e
doron <doron.miller@gmail.com>
2019-07-04 19:02:28 +0300

    master - refactoring to use the addOn as a package

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
 # FT245RL:
 
-**This project is adding and using addON for controling FT245RL
+**This project is using addON for controling FT245RL
 device.**
 
-**It relies on the ftdi package:  [ftdi](https://github.com/thomaschaaf/node-ftdi)**
+**The addOn [FT245RL](https://github.com/doron-mil/node-FT245RL) 
+relies on the ftdi package:  
+[ftdi](https://github.com/thomaschaaf/node-ftdi)**
 
-# Installation
-    
-    npm npm clean_install
-
-# Running ( after connecting the device)
-
-    npm start
 
 # Modifications:
 
 * The index.js is to use the addOn
-* The FT245RL/index.js is the interface to communicate with the C++.
 
-You can change those two (Or more...) according to your needs.
 
--- index.js
+++ index.js
@@ -14,42 +14,50 @@ ftdi.findFirst().then((device) => {
     });
     console.log('11111', device);
 
-    let data = 0;
-    let counter = -1;
-    let itCounter = 0;
-    let a2 = 0;
-    let prev = 0;
-    let allOn = true ;
-    let interval = setInterval(function () {
-        counter++;
-        if (data >= 15) {
-            data = 0;
-            itCounter++;
+    ftdi.openDevice(device, (err) => {
+        if (err) {
+            console.error('Failed to open', err);
+            return;
         }
-        const mod = counter % 4;
-        const itMod = itCounter % 2;
-        const a1 = 1 << mod;
-        const a3 = a1 | data;
-        a2 = (a3 !== data) ? a3 : a2;
-        data = (mod == 3) ? a2 : data;
-        if (a2 != prev) {
-            const dataArray = Array.from(a2.toString(2).padStart(4, '0'))
-                .map(x => Number.parseInt(x));
-            if (itMod === 0) {
-                dataArray.reverse();
-            }
-            ftdi.switchPorts(device, dataArray, (err) => {
-                console.log('*********** ', dataArray);
-            });
-        }else{
-           ftdi.switchAllPorts(device, allOn , (err) => {
-                console.log('*********** ', itMod);
-                allOn = !allOn;
-            });
-        }
-        prev = a2;
 
-    }, 100);
+
+        let data = 0;
+        let counter = -1;
+        let itCounter = 0;
+        let a2 = 0;
+        let prev = 0;
+        let allOn = true;
+        let interval = setInterval(function () {
+            counter++;
+            if (data >= 15) {
+                data = 0;
+                itCounter++;
+            }
+            const mod = counter % 4;
+            const itMod = itCounter % 2;
+            const a1 = 1 << mod;
+            const a3 = a1 | data;
+            a2 = (a3 !== data) ? a3 : a2;
+            data = (mod == 3) ? a2 : data;
+            if (a2 != prev) {
+                const dataArray = Array.from(a2.toString(2).padStart(4, '0'))
+                    .map(x => Number.parseInt(x));
+                if (itMod === 0) {
+                    dataArray.reverse();
+                }
+                ftdi.switchPorts(device, dataArray, (err) => {
+                    console.log('*********** ', dataArray);
+                });
+            } else {
+                ftdi.switchAllPorts(device, allOn, (err) => {
+                    console.log('*********** ', itMod);
+                    allOn = !allOn;
+                });
+            }
+            prev = a2;
+
+        }, 100);
+    });
 
 }).catch((err) => {
     console.error('ERRRrr', err);
