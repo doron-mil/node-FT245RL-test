b0dd58db2982be5798f233e527b048a2946185e2
doron <doron.miller@gmail.com>
2019-11-03 15:28:41 +0200

    Responding to using the ft245rl npm package

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const ftdi = require('FT245RL');
+const ftdi = require('ft245rl');
 
 const funcMap = [];
+
+const setOnDeviceListeners = (aDevice) => {
+
+    aDevice.on('error', function (err) {
+        console.log('ON ERROR !!!', err);
+        return;
+    });
+    aDevice.on('open', function () {
+        console.log('ON OPEN !!!');
+        return;
+    });
+    aDevice.on('data', function (data) {
+        console.log('ON DATA !!!', data);
+        return;
+    });
+    aDevice.on('close', function (data) {
+        console.log('ON CLOSE !!!', data);
+        return;
+    });
+    console.log('GO DEVICE !!!', aDevice);
+};
 
 funcMap[0] = () => ftdi.find(0x403, 0x6001, function (err, devices) {
     console.log('ddddd', devices.length, devices[0]);
@@ -15,17 +36,9 @@ const card2_productId = 0x6001;
 const card2_serialNumber = 'A601AXC8';
 
 funcMap[1] = (aPortsCount) => ftdi.findFirst().then((device) => {
-    device.on('error', function (err) {
-        console.log('8888', err);
-    });
-    console.log('11111', device);
+    setOnDeviceListeners(device);
 
-    ftdi.openDevice(device, (err) => {
-        if (err) {
-            console.error('Failed to open', err);
-            return;
-        }
-
+    ftdi.openDevice(device).then(() => {
 
         let data = 0;
         let counter = -1;
@@ -44,30 +57,36 @@ funcMap[1] = (aPortsCount) => ftdi.findFirst().then((device) => {
             const a1 = 1 << mod;
             const a3 = a1 | data;
             a2 = (a3 !== data) ? a3 : a2;
-            data = (mod === aPortsCount-1) ? a2 : data;
+            data = (mod === aPortsCount - 1) ? a2 : data;
             if (a2 != prev) {
                 const dataArray = Array.from(a2.toString(2).padStart(aPortsCount, '0'))
                     .map(x => Number.parseInt(x));
                 if (itMod === 0) {
                     dataArray.reverse();
                 }
-                ftdi.switchPorts(device, dataArray, (err) => {
+                ftdi.switchPorts(device, dataArray).then(() => {
                     console.log('*********** ', dataArray);
+                }).catch((err) => {
+                    console.error('Failed to switchPorts !!!!', dataArray);
                 });
             } else {
-                ftdi.switchAllPorts(device, allOn, (err) => {
+                ftdi.switchAllPorts(device, allOn).then(() => {
                     console.log('All******** ', itMod);
                     allOn = !allOn;
+                }).catch((err) => {
+                    console.error('Failed to switchAllPorts  !!!! on : ', allOn);
                 });
             }
             prev = a2;
 
-        }, 2000);
+        }, 750);
+    }).catch((err) => {
+        console.error('Failed to open !!!!');
     });
 
 }).catch((err) => {
     console.error('ERRRrr', err);
 });
 
-funcMap[0]();
-// funcMap[1](8);
+// funcMap[0]();
+funcMap[1](8);
